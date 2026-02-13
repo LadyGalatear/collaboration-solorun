457f344ad6755714b258befaeeedb853e86acd6c
Galatear <171226854+LadyGalatear@users.noreply.github.com>
2026-02-12 20:36:48 -0800

    Add admin

--- app.js
+++ app.js
@@ -6,9 +6,37 @@ const PORT = 5000;
 
 app.use(express.static('public'));
 
-//define a default route
+app.use(express.urlencoded ({ extended: true }))
+
+const appointments = [];
+
+// define a default route
 app.get('/', (req, res) => {
     res.sendFile(`${import.meta.dirname}/views/home.html`);
+});
+
+// Confirmation route
+app.get('/confirmation', (req, res) => {
+    res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
+});
+
+// Admin route
+app.get('/admin', (req, res) => {
+    res.send(appointments);
+});
+
+app.post('/submit-app', (req, res) => {
+    const appointment = {
+        fname: req.body.fname,
+        lname: req.body.lname,
+        date: req.body.date,
+        time: req.body.time,
+        timestamp: new Date()
+    };
+
+    appointments.push(appointment);
+
+    res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
 });
 
 app.listen(PORT, () => {
